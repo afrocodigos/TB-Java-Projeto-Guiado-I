81fd67e142065bb890fd7ad61788ed68cabb36fc
amaral220x <biel.ilha2021@gmail.com>
2024-04-25 09:56:31 -0300

    feat: Atualizar produtos (Update)

--- src/juliaoStore/Estoque.java
+++ src/juliaoStore/Estoque.java
@@ -1,5 +1,6 @@
 package juliaoStore;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Estoque {
     //atributos
@@ -22,9 +23,55 @@ public class Estoque {
             System.out.println("Nenhum produto em estoque");
         } else {
             System.out.println("Produtos em estoque: ");
+            int aux = 1;
             for (Produto produto : produtos) {
-                System.out.println("Nome: " + produto.getNome() + ", Quantidade: " + produto.getQuantidade() + ", Preço: " + produto.getPreco());
+                System.out.println("-"+aux+": "+produto);
+                aux++;
             }
         }
     }
+
+
+    // U - Update (Atualizar)
+    public void atualizarProduto() {
+        if(produtos.isEmpty()){
+            System.out.println("Nenhum produto em estoque");
+        } else {
+            Scanner s = new Scanner(System.in);
+            listarProdutos();
+            System.out.println("Digite o número do produto que deseja atualizar: ");
+            int index = s.nextInt();
+            if(index > produtos.size() || index <= 0){
+                System.out.println("Produto não encontrado");
+            } else {
+                System.out.println("Produto selecionado: "+produtos.get(index-1));
+                optionsUpdate();
+                int option = s.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println("Digite o novo nome: ");
+                        String nome = s.next();
+                        produtos.get(index-1).setNome(nome);
+                        System.out.println("Nome atualizado com sucesso!");
+                        break;
+                    case 2:
+                        System.out.println("Digite a nova quantidade: ");
+                        int quantidade = s.nextInt();
+                        produtos.get(index-1).setQuantidade(quantidade);
+                        System.out.println("Quantidade atualizada com sucesso!");
+                        break;
+                    case 3:
+                        System.out.println("Digite o novo preço: ");
+                        double preco = s.nextDouble();
+                        produtos.get(index-1).setPreco(preco);
+                        System.out.println("Preço atualizado com sucesso!");
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                        break;
+                }
+            }
+        }
+    }
+
 }
