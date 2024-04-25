c99a30f3c1ba545bf941128b9a7f61a8a3f50379
amaral220x <biel.ilha2021@gmail.com>
2024-04-25 09:54:09 -0300

    feat: Sobrescrita do toString em Produto.

--- src/juliaoStore/Produto.java
+++ src/juliaoStore/Produto.java
@@ -41,4 +41,9 @@ public class Produto {
     public void setPreco(double preco) {
         this.preco = preco;
     }
+
+    @Override
+    public String toString(){
+        return "Produto: " + nome + " | Quantidade: " + quantidade + " | Preço: " + preco;
+    }
 }
