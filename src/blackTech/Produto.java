7e4eedb145d4f84eacca6f226283119f6b2fd6af
hugomacielads <hugomacielcesar@gmail.com>
2024-04-25 23:56:50 -0300

    feat : add classe Produto, Cliente e Venda

--- src/blackTech/Produto.java
+++ src/blackTech/Produto.java
@@ -1,4 +1,57 @@
 package blackTech;
 
 public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private int estoque;
+    private int estoqueMinimo;
+
+    public Produto(int id, String nome, double preco, int estoque, int estoqueMinimo) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public int getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(int estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
 }
---
+++ src/blackTech/Venda.java
@@ -0,0 +1,47 @@
+package blackTech;
+
+public class Venda {
+    private int id;
+    private Cliente cliente;
+    private Produto produto;
+    private int quantidade;
+
+    public Venda(int id, Cliente cliente, Produto produto, int quantidade) {
+        this.id = id;
+        this.cliente = cliente;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
