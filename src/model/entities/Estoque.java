46e4edf9fb242d1e803998b095f4c01dc698850f
Lucas Teles <lucasouzateles@gmail.com>
2024-04-28 21:43:03 -0300

    :sparkles: feat: Finalizando o projeto para entrega !

--- src/model/entities/Estoque.java
+++ src/model/entities/Estoque.java
@@ -30,21 +30,49 @@ public class Estoque {
         }
     }
 
-
-    public void atualizarProduto(String nome, int quantidade, double preco){
-        if (produtos.isEmpty()) {
-            throw new Excecoes("Não há produtos cadastrados");
-        }
-
+    public boolean verificaNome(String nome){
         for (Produto produto : produtos) {
-            if (produto.getNome().equals(nome)) {
+                if (produto.getNome().equals(nome)) {
+                    return true;
+                }
+            }
+        throw new Excecoes("Produto não encontrado no estoque.");
+    }
+
+
+
+    public void atualizarProduto(String nome, int quantidade, double preco) {
+        for (Produto produto : produtos) {
+            if(produto.getNome().equals(nome)) {
                 produto.setQuantidade(quantidade);
                 produto.setPreco(preco);
-                System.out.println(nome + "atualizado");
+
+                if(produto.getQuantidade() <= 0) {
+                    System.out.println("Devido o estoque ter sido zerado, item foi removido.");
+                    removerProduto((nome));
+                    return;
+                }
+                System.out.println("O produto ->" + produto.getNome() + " foi atualizado com sucesso");
                 return;
             }
         }
-        throw new Excecoes("Produto '" + nome + "' não encontrado");
+        System.out.println("Produto informado não existe em estoque");
+    }
+
+    public void retirarProduto(String nome, int quantidade){
+        for (Produto produto : produtos) {
+            if (produto.getNome().equals(nome)) {
+                if (produto.getQuantidade() >= quantidade) {
+                    produto.setQuantidade(produto.getQuantidade() - quantidade);
+                    System.out.println(quantidade + " unidades de " + nome + " foram retiradas do estoque.");
+                    return;
+                } else {
+                    System.out.println("Não há quantidade suficiente de " + nome + " em estoque para retirar.");
+                    return;
+                }
+            }
+        }
+        System.out.println("Produto '" + nome + "' não encontrado no estoque.");
     }
 
 
--- src/model/entities/Util.java
+++ src/model/entities/Util.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.Excecoes;
+
 import java.util.Scanner;
 
 public class Util {
@@ -8,9 +10,32 @@ public class Util {
         System.out.println("----------   Bem-vindo à LucStorage   ----------");
         System.out.println(" -- O QUE DESEJA REALIZAR? --");
         System.out.println("1 - Adicionar item ao storage | 2 - Ver o storage");
-        System.out.println("3 - Atualizar item do storage | 4 - Remover item");
-        System.out.println("5 - Para sair");
-        System.out.print("--> ");
+        System.out.println("3 - Atualizar item do storage | 4 - Para retirar item");
+        System.out.println("5 - Para remover item         | 0 - Para sair");
     }
 
+    public int obterOpcaoDoUsuario(Scanner scanner) {
+        System.out.print("--> ");
+        return scanner.nextInt();
+    }
+
+    public String obterNomeDoProduto(Scanner scanner) {
+        System.out.print("Nome do produto: ");
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+
+    public int obterQuantidade(Scanner scanner) {
+        System.out.print("Quantidade: ");
+        return scanner.nextInt();
+    }
+
+    public double obterPreco(Scanner scanner) {
+        System.out.print("Preço UN: R$ ");
+        return scanner.nextDouble();
+    }
+
+    public String mensagemErro(){
+        throw new Excecoes("Opção inválida, verifique se digitou corretamente. Até mais !");
+    }
 }
