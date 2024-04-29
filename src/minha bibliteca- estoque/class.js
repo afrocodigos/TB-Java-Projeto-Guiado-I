4bd9b88ab71138fd329ede00f9f0e1d3d6a1e0d5
victoria.clube <victoria.clube@gmail.com>
2024-04-28 21:27:27 -0300

    projetoguiadovictoriasalino

---
+++ src/minha bibliteca- estoque/class.js
@@ -0,0 +1,9 @@
+class biblioteca {
+    constructor(titulo, autor, quantidade) {
+        this._titulo = titulo;
+        this.autor = autor;
+        this.__quantidade = quantidade;
+    }
+
+    // Outros métodos da classe
+}
