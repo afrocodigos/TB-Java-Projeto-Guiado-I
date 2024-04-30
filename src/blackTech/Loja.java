2c742fb2dbefb8fa1815b9bb0d8c4510d25078d1
hugomacielads <hugomacielcesar@gmail.com>
2024-04-30 10:24:12 -0300

    feat : add método ExibirMenu()

--- src/blackTech/Loja.java
+++ src/blackTech/Loja.java
@@ -3,6 +3,8 @@ package blackTech;
 import java.util.*;
 import java.util.Scanner;
 
+import static blackTech.Menu.ExibirMenu;
+
 public class Loja {
     private static List<Produto> produtos = new ArrayList<>();
     private static List<Cliente> clientes = new ArrayList<>();
@@ -18,30 +20,7 @@ public class Loja {
         //  Menu interativo que permita ao usuário escolher e realizar todas as funcionalidades disponíveis
         do {
             try {
-                System.out.println("Menu:");
-
-                // Métodos de Criação (Create)
-                System.out.println("1. Cadastrar Produto");
-                // Cadastro de Eletrônico, classe filha da classe produto
-                System.out.println("2. Cadastrar Eletrônico");
-                System.out.println("3. Cadastrar Cliente");
-                System.out.println("4. Realizar Venda");
-
-                // Métodos de Leitura (Read)
-                System.out.println("5. Listar Produtos");
-                System.out.println("6. Listar Vendas");
-                System.out.println("7. Listar Clientes");
-                System.out.println("8. Relatório de Faturamento");
-                System.out.println("9. Relatório de Estoque Mínimo");
-
-                // Métodos de Edição (Update)
-                System.out.println("10. Editar Produtos");
-
-                // Métodos de Exclusão (Delete)
-                System.out.println("11. Remover Produtos");
-
-                System.out.println("0. Sair");
-                System.out.print("Escolha uma opção: ");
+                ExibirMenu();
                 opcao = scanner.nextInt();
 
                 switch (opcao) {
