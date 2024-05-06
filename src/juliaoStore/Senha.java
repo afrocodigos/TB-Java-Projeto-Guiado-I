d101f1301681e6e61eea7f08ffc61e50fcb4e8fa
heloisebastos <heloisebastos28@hotmail.com>
2024-05-05 23:10:35 -0400

    :recycle: refactor: Transforma o atributo senha em uma constate estática

--- src/juliaoStore/Senha.java
+++ src/juliaoStore/Senha.java
@@ -2,18 +2,10 @@ package juliaoStore;
 
 public class Senha {
 
-    private String senha = "af123";
+    private static final String senha = "af123";
 
-    public Senha() {
-
-    }
-
-    public String getSenha() {
+    public static String getSenha() {
         return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 
 }
